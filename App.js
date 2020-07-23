02d6aadaf68f53ff80ac0bca6299445e39e161a8
abechoi <abraham.choi@icloud.com>
2020-07-23 16:56:18 -0700

    Text Inputs

--- App.js
+++ App.js
@@ -1,25 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState }from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
 
   const [name, setName] = useState('Choi Jong-Hae');
-  const [person, setPerson] = useState({ name: 'Mario', age: 40});
-
-  const handlePress = () => {
-    setName('Abe Choi');
-    setPerson({ name: 'Luigi', age: 45});
-  }
+  const [age, setAge] = useState(33);
 
   return (
     <View style={styles.container}>
-      <Text>My Korean name is { name }.</Text>
-      <Text>His name is {person.name} and his age is {person.age}</Text>
-      <View style={styles.buttonContainer}>
-        <Button title="Update Name" onPress={handlePress} />
-      </View>
-      <StatusBar style="auto"/>
+      <Text> Enter name: </Text>
+      <TextInput 
+        multiline
+        style={styles.input}
+        placeholder="e.g. John Doe"
+        onChangeText={(val) => setName(val)}/>
+        <Text> Enter name: </Text>
+      <TextInput 
+        style={styles.input}
+        keyboardType='numeric'
+        placeholder="e.g. 99"
+        onChangeText={(val) => setAge(val)}/>
+      <Text>name: {name}, age: {age}</Text>
     </View>
   );
 }
@@ -31,18 +33,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  buttonContainer: {
-    marginTop: 20
-  },
-  header: {
-    backgroundColor: 'pink',
-    padding: 20,
-  },
-  boldText: {
-    fontWeight: 'bold',
-  },
-  body: {
-    backgroundColor: 'yellow',
-    padding: 20
+  input: {
+    borderWidth: 1,
+    borderColor: '#777',
+    padding: 8,
+    margin: 10,
+    width: 200
   }
 });
